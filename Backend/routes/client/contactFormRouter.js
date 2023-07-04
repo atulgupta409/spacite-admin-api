a7d8b75c1d6bee9ace39a167ddf65e081cc97a66
Devansh-bharadwaj <devanshbharadwaj1@gmail.com>
2023-07-04 10:26:06 +0530

    nodemailer added

---
+++ Backend/routes/client/contactFormRouter.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+
+router.post("/sendmail", (req, res) => {
+  const { name, email, phone, office_type, no_of_seats, move_in } = req.body;
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL,
+      subject: "coworking mail",
+      html: `<ul>
+       <li>Name: ${name}</li>
+       <li>Email: ${email}</li>
+       <li>Phone: ${phone}</li>
+       <li>Office Type: ${office_type}</li>
+       <li>No. of seats: ${no_of_seats}</li>
+       <li>Move in: ${move_in}</li>
+      </ul>`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("Error", error);
+      } else {
+        console.log("Email sent" + info.response);
+        res.status(201).json({ status: 201, info });
+      }
+    });
+  } catch (error) {
+    res.status(401).json({ status: 401, error });
+  }
+});
+
+module.exports = router;
